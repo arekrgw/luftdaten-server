cd6cef0d1a40d02528deb3d50e29024292649f5c
ArekRGW <arkadiusz.pawlak.web@gmail.com>
2020-02-21 14:05:52 +0100

    Hotfix round pressure

--- index.js
+++ index.js
@@ -22,22 +22,22 @@ else{
         console.log(req.body)
         //Save air condition
         fs.writeFile(
-          `${argv.path}/air_condition_${id}`,
+          `${argv.path}/air_condition_${id}.txt`,
           `${req.body.sensordatavalues[0].value}\n${req.body.sensordatavalues[1].value}`,
           err => {
             console.log("Air condition saved");
           }
         );
         fs.writeFile(
-          `${argv.path}/temp_and_humidity_${id}`,
+          `${argv.path}/temp_and_humidity_${id}.txt`,
           `${req.body.sensordatavalues[2].value}\n${req.body.sensordatavalues[4].value}`,
           err => {
             console.log("Temperature and humidity saved");
           }
         );
         fs.writeFile(
-          `${argv.path}/pressure_${id}`,
-          `${req.body.sensordatavalues[3].value}`,
+          `${argv.path}/pressure_${id}.txt`,
+          `${Math.round((req.body.sensordatavalues[3].value + Number.EPSILON)) / 100}`,
           err => {
             console.log("Pressure saved");
           }
