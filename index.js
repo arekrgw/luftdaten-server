9fdedaa99b11c0edf43e70a3d895798bea812925
ArekRGW <arkadiusz.pawlak.web@gmail.com>
2020-02-21 13:49:08 +0100

    Added command line args

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const express = require("express")
 const app = express()
-const port = 5125
+const argv = require("yargs").argv
+const port = argv.port
 const fs = require("fs")
 app.use(express.json())
+console.log("Init...")
 
-console.log("Init...\nListening on port 5125")
+if(!argv.path) {
+    console.log("You have to provide path to destination folder")
+}
+else if(!argv.port){
+    console.log("You have to provide port");
+}
+else{
+    console.log(`Listening on port ${port}`)
 
-app.post("/sensor/:id", (req, res) => {
-    console.log(req.body)
-    fs.writeFile("luft.txt", `${req.body.sensordatavalues[0].value}\n${req.body.sensordatavalues[1].values}`, (err) => {
-        console.log("file saved")
+
+    app.post("/sensor/:id", (req, res) => {
+        let id = req.params.id ? req.params.id : Math.floor(Math.random() * (100000 - 1000 + 1) + 1000)
+
+        console.log(req.body)
+        //Save air condition
+        fs.writeFile(
+          `${path}/air_condition_${id}`,
+          `${req.body.sensordatavalues[0].value}\n${req.body.sensordatavalues[1].value}`,
+          err => {
+            console.log("file saved");
+          }
+        );
     })
-    res.send("WORKS")
-})
+    
+    app.listen(port)
 
-app.listen(port)
+}
+
+
